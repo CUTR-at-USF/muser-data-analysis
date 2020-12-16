7c532da8b29a8e8a885bb65dec4ff503310413cf
Ankit Raj <srivastavaankit667@gmail.com>
2020-12-17 00:53:41 +0530

    Chore: Added comments for the scripts, removed hardcoded path

--- AI/models.py
+++ AI/models.py
@@ -1,11 +1,31 @@
+'''
+/*
+ * Copyright (C) 2019-2020 University of South Florida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ '''
+
+# Import the dependencies
 import pandas as pd
 from textslack.textslack import TextSlack
 from gensim.models import doc2vec
-import os
 
-os.chdir(r'C:\Users\sriva\Desktop\edu.usf.sas.pal.muser\SpotifyDataExtractor')
 
+# Architecture of the NLP Model
 class NLPModel:
+
+    # The constructor instantiates all the variables that would be used throughout the class
     def __init__(self, sp, conn, max_epochs=100, vec_size=50, alpha=0.025):
         self.sp = sp
         self.conn = conn
@@ -15,16 +35,19 @@ class NLPModel:
         self.alpha = alpha
         self.df = pd.read_sql_table('SPOTIFY_DATA', con=self.conn)
 
+    # Function that tags the list of words with indices
     def _create_tagged_document(self, list_of_list_of_words):
         for i, list_of_words in enumerate(list_of_list_of_words):
             yield doc2vec.TaggedDocument(list_of_words, [i])
 
+    # Function to prepare the training data
     def _training_data(self):
         key_features = (self.df['album'] + ' ' + self.df['name'] + ' ' + self.df['artist']).tolist()
         cleaned_key_features = self.slack.transform(key_features)
         list_list_words = [sent.split() for sent in cleaned_key_features]
         return list_list_words
 
+    # Function to build and train the model
     def build_model(self):
         list_list_words = self._training_data()
         train_data = list(self._create_tagged_document(list_list_words))
@@ -46,6 +69,7 @@ class NLPModel:
         model.save('d2v.model')
         print("Model Saved")
 
+    # Function to predict the most similar doc in the doc2vec model
     def most_similar_doc(self, target):
         model = doc2vec.Doc2Vec.load('d2v.model')
         model.random.seed(95)
