701aef3fc2db99556a1dc8febc407ace746b54a2
Ankit Raj <srivastavaankit667@gmail.com>
2020-12-17 01:05:29 +0530

    Chore: No updates

--- config.py
+++ config.py
@@ -1,3 +1,24 @@
+'''
+/*
+ * Copyright (C) 2019-2020 University of South Florida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ '''
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     """
     Use this class to share any default attributes with any subsequent
@@ -10,7 +31,6 @@ class Config:
     # Generated with secrets.token_urlsafe(16)
     # You could also use os.urandom(16)
     SECRET_KEY = "muser_data_app"
-
 
 class ProductionConfig(Config):
     """
