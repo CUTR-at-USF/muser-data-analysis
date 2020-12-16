f61d8e4e35300194045853ced016d4c6e8971241
Ankit Raj <srivastavaankit667@gmail.com>
2020-12-17 01:02:18 +0530

    Chore: Hide the secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+# Import the dependencies
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+# Sets the correct .env file
+load_dotenv(os.path.join(os.getcwd(), '.env'))
+
+# Creates a flask app
 app = Flask(__name__)
 
-app.secret_key = "muser_data_app"
+# Makes call to the config file
+app.secret_key = os.environ.get('SECRET_KEY')
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
 elif app.config["ENV"] == "development":
@@ -10,4 +18,5 @@ elif app.config["ENV"] == "development":
 else:
     app.config.from_object("config.ProductionConfig")
 
+# Imports the view.py file
 from app import view
